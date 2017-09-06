d5ee5ea55619cbb6c507424ae0d5e4a7f1a5107d
ajoergensen <31108516+ajoergensen@users.noreply.github.com>
2017-09-06 21:03:29 +0200

    Remove dsa and ecdsa keys

--- root/etc/cont-init.d/91_ssh_setup.sh
+++ root/etc/cont-init.d/91_ssh_setup.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/with-contenv bash
 set -eo pipefail
 shopt -s nocasematch
+if [[ $DEBUG == "true" ]]
+ then
+	set -x
+fi
 
 : ${DISABLE_KEYGEN:="false"}
 : ${DISABLE_CONFIG_GEN:="false"}
 
 if [[ $DISABLE_KEYGEN == "false" ]]
  then
-	for t in rsa dsa ecdsa ed25519
+	for t in rsa ed25519
 	 do
 		f="/etc/ssh/keys/ssh_host_${t}_key"
 		if [[ ! -f $f ]]
 		 then
-			ssh-keygen -t $t -f $f -N ''
+			ssh-keygen -t $t -f $f -N '' < /dev/null
 		fi
 	done
 fi
